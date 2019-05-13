5363cf2fa325d2d883cd7eec064b6016868874aa
Xavier Olivares <xm.olivares@gmail.com>
2019-05-13 10:35:36 -0400

    candies model base, route pending

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -2,5 +2,21 @@ const Sequelize = require('sequelize');
 const db = require('../database');
 
 module.exports = db.define('candy', {
+  name: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      max: 10
+    }
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    defaultValue: 'png'
+  }
   // define your model here!
 });
