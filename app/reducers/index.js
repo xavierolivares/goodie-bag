2325a286d746872286fcab82224e7279542cdbd1
Xavier Olivares <xm.olivares@gmail.com>
2019-05-13 11:29:38 -0400

    built thunk creator, action types, and getting candies

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,34 @@
+import Axios from "axios";
 
-const initialState = {}
+const initialState = {
+  candies: []
+}
+
+const GOT_CANDIES = 'GOT_CANDIES'
+
+const gotCandies = candies => {
+  return {
+    type: GOT_CANDIES,
+    candies
+  }
+}
+
+export const gettingCandies = () => {
+  return async dispatch => {
+    try {
+      const { data } = await Axios.get('/api/candies')
+      dispatch(gotCandies(data)) 
+    } catch (error) {
+      console.log('Houston, we have a problem')
+    }
+  }
+}
+
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GOT_CANDIES:
+    return {...state, candies: [...state.candies, action.candies]}
     default:
       return state
   }
